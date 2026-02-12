a7481e4f6554bae59675d44115d16353702e4ced
NamanAshani <namanashani551@gmail.com>
2026-02-12 22:43:40 +0530

    [imp]stock and prodect database implemented.In order,customer,stock database total_quantity feature updated

--- FactoryInventoryManagmentSystem/customer/models.py
+++ FactoryInventoryManagmentSystem/customer/models.py
@@ -2,19 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    Customer_name = models.CharField(max_length=100)
-    phone_no = models.CharField(max_length=15)
-    Customer_city = models.CharField(max_length=50)
-    Customer_date = models.DateTimeField(auto_now_add=True)
     customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=15)
+    customer_city = models.CharField(max_length=50)
+    customer_date = models.DateTimeField(auto_now_add=True)
     cus_quantity = models.IntegerField(default=0)
-    mark = models.CharField(max_length=255)
     remark = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
     truck_no = models.CharField(max_length=50)
     ref_party = models.CharField(max_length=255)
 
 
     def __str__(self):
-        return self.Customer_name
+        return self.customer_name
