837005349e0ed20cae26d1e38de198e46a5620b0
Ronit-Vyas <ronitvyas85@gmail.com>
2026-02-14 09:22:05 +0530

    [imp]View Stock frontend

--- FactoryInventoryManagmentSystem/FactoryInventoryManagmentSystem/urls.py
+++ FactoryInventoryManagmentSystem/FactoryInventoryManagmentSystem/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('order.urls')),
+    path('stock/', include('stock.urls')),
 ]
---
+++ FactoryInventoryManagmentSystem/stock/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path("login/<str:role>/", views.login_view, name="login"),
+   
+]
