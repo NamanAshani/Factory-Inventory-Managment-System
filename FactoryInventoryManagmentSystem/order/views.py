17ba9d3754249cc2dd300b7e9dff2eafd02efd8f
PruthaAghara <agharaprutha@gmail.com>
2026-03-06 09:15:55 +0530

    Updated order views

--- FactoryInventoryManagmentSystem/order/views.py
+++ FactoryInventoryManagmentSystem/order/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
 def order_home(request):
-=======
-def index(request):
->>>>>>> 837005349e0ed20cae26d1e38de198e46a5620b0
     return render(request, "order/index.html")
