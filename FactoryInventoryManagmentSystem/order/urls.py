6f640d31ff13ebaef37bd429be311966d01177b4
PruthaAghara <agharaprutha@gmail.com>
2026-03-06 10:06:54 +0530

    Updated order urls

--- FactoryInventoryManagmentSystem/order/urls.py
+++ FactoryInventoryManagmentSystem/order/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
      path("", views.order_home, name="order_home"),
     path("create/", views.order_create, name="order_create"),
+    path("<int:pk>/update/", views.order_update, name="order_update"),
 ]
