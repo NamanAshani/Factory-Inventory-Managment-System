652a5890cb395316147a463660128e4a0b7fb771
PruthaAghara <agharaprutha@gmail.com>
2026-03-06 09:12:05 +0530

    Updated order urls configuration

--- FactoryInventoryManagmentSystem/order/urls.py
+++ FactoryInventoryManagmentSystem/order/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", views.order_home, name="order_home"),
-=======
-    path('', views.index, name='index'),
-    # path("login/<str:role>/", views.login_view, name="login"),
-   
->>>>>>> 837005349e0ed20cae26d1e38de198e46a5620b0
+    path("", views.index, name="index"),
+    path("order_home/", views.order_home, name="order_home"),
 ]
